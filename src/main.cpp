4401cb175605a81b334a8aba3703d1309e6afe79
kritomas <Unknown>
2024-02-09 21:31:57 +0100

    Added initial interactivity with stdin/stdout.

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,48 @@
 #include <fstream>
 #include <sstream>
 
+void usage()
+{
+	std::cout << "Usage:\n";
+	std::cout << "kritase64 encode [FILE]\n";
+	std::cout << "kritase64 decode [base64 string]\n";
+	std::cout << "If no FILE or base64 string is provided, input is taken from standard input. Output is to standard output." << std::endl;
+}
+
 int main(int argc, char* argv[])
 {
-	//std::stringstream stream("s")
-	kritase64::Stream stream("aGVsbG8gd29ybGQ=", std::ios::in | std::ios::out | std::ios::ate);
+	if (argc <= 1)
+	{
+		usage();
+		return 0;
+	}
 
-	stream << "hello world";
-
-	std::cout << stream.base64() << std::endl;
-
+	if (argv[1] == (std::string)"encode")
+	{
+		kritase64::Stream stream("", std::ios::out | std::ios::binary);
+		while (!std::cin.eof())
+		{
+			char c;
+			std::cin.read(&c, 1);
+			if (!std::cin.eof()) stream.write(&c, 1);
+		}
+		std::cout << stream.base64();
+	}
+	else if (argv[1] == (std::string)"decode")
+	{
+		std::string base64;
+		while (!std::cin.eof())
+		{
+			char c;
+			std::cin.read(&c, 1);
+			if (!std::cin.eof()) base64.push_back(c);
+		}
+		kritase64::Buffer data = kritase64::decode(base64);
+		std::cout.write((char*)data.data(), data.size());
+	}
+	else
+	{
+		usage();
+	}
 	return 0;
 }
