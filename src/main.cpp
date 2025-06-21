f260c89b6e439142593df5006bfaf228826dfec0
kritomas <Unknown>
2025-06-21 19:55:56 +0200

    Bumped version.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /*
  *	MIT License
  *
- *	Copyright (c) 2024 kritomas
+ *	Copyright (c) 2025 kritomas
  *
  *	Permission is hereby granted, free of charge, to any person obtaining a copy
  *	of this software and associated documentation files (the "Software"), to deal
@@ -164,14 +164,14 @@ int main(int argc, char* argv[])
 		if (argv[1] == (std::string)"version")
 		{
 			std::cout << PACKAGE_NAME << " v" << VERSION << "\n";
-			std::cout << "Copyright (c) 2024 kritomas\n";
+			std::cout << "Copyright (c) 2025 kritomas\n";
 			std::cout << "This project is licensed under the MIT License. See source code for details: https://gitlab.com/kritomas/kritase64\n";
-			std::cout <<	"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR "
-							"IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, "
-							"FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE "
-							"AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER "
-							"LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, "
-							"OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE "
+			std::cout <<	"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"
+							"IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n"
+							"FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n"
+							"AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n"
+							"LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n"
+							"OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n"
 							"SOFTWARE." << std::endl;
 		}
 		else if (argv[1] == (std::string)"encode")
